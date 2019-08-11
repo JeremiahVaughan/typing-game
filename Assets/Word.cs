9dd27fd91cce6e4b62219ae99ae89f6e0b5b72a2
Jeremiah Vaughan <jeremiah.vaughan1@student.ctuonline.edu>
2019-08-11 12:30:38 -0500

    first commit

---
+++ Assets/Word.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Word {
+
+    public string word;
+    private int typeIndex;
+
+    WordDisplay wordDisplay;
+
+    public Word(string word, WordDisplay wordDisplay)
+    {
+        this.word = word;
+        typeIndex = 0;
+
+        this.wordDisplay = wordDisplay;
+        wordDisplay.SetWord(word);
+    }
+
+    public char GetNextLetter()
+    {
+        return word[typeIndex];
+    }
+}
---
+++ Assets/WordManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordManager : MonoBehaviour {
+
+    public List<Word> words;
+
+    public WordSpawner wordSpawner;
+
+    private void Start()
+    {
+        AddWord();
+        AddWord();
+        AddWord();
+    }
+
+    private void AddWord()
+    {
+        WordDisplay wordDisplay = wordSpawner.SpawnWord();
+
+        Word word = new Word(WordGenerator.getRandomWord(), wordDisplay);
+        Debug.Log(word.word);
+
+        words.Add(word);
+    }
+
+    public void TypeLetter(char letter)
+    {
+
+    }
+    
+}
